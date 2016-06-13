56217646d65e467e9609170b9470d65104bcbc68
Cauê Alves <cauealveseng@gmail.com>
2016-06-13 00:33:04 -0300

    Some refactoring and use space indent

--- pagespeed.js
+++ pagespeed.js
@@ -7,14 +7,14 @@ const fetch = require('node-fetch');
 const API_URL = 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?';
 
 module.exports = function (url, strategy, locale, filterThirdParty) {
-	let query = [
-		'url=' + url,
-		'strategy=' + strategy,
-		'locale=' + locale,
-		'filter_third_party_resources=' + filterThirdParty
-	].join('&');
+  let query = [
+    'url=' + url,
+    'strategy=' + strategy,
+    'locale=' + locale,
+    'filter_third_party_resources=' + filterThirdParty
+  ].join('&');
 
-	return fetch(API_URL + query).then((response) => {
-		return response.json();
-	});
+  return fetch(API_URL + query).then((response) => {
+    return response.json();
+  });
 };
--- test.js
+++ test.js
@@ -2,12 +2,12 @@ import childProcess from 'child_process';
 import test from 'ava';
 
 test.cb(t => {
-	const cp = childProcess.spawn('./cli.js', {stdio: 'inherit'});
+  const cp = childProcess.spawn('./cli.js', {stdio: 'inherit'});
 
-	cp.on('error', t.ifError);
+  cp.on('error', t.ifError);
 
-	cp.on('close', code => {
-		t.is(code, 1);
-		t.end();
-	});
+  cp.on('close', code => {
+    t.is(code, 0);
+    t.end();
+  });
 });
