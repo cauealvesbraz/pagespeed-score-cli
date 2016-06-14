bc8610384fc9c212b3c20b0bad9ec2042331524b
Cauê Alves <cauealveseng@gmail.com>
2016-06-13 22:02:58 -0300

    Remove the network connection test

--- test.js
+++ test.js
@@ -4,9 +4,3 @@ import execa from 'execa';
 test('should have an error with an invalid URL', t => {
   t.throws(execa('./cli.js', ['cauealves.com']), /The given URL was not valid./);
 });
-
-test('should have an error without network connect', async t => {
-  await execa('ifconfig', ['eth0', 'down']).then(() => {
-    t.throws(execa('./cli.js', ['http://cauealves.com']), /Please check you internet connection./);
-  });
-});
