3923b78869269ce4872a28fd84dcc730d921049a
Cauê Alves <cauealveseng@gmail.com>
2016-06-15 01:31:59 -0300

    Fix typo os test

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ test('should show the version when pass a parameter --version', async t => {
   t.is(await execa.stdout('./cli.js', ['--version']), version);
 });
 
-test('should have an error when a domain is not valid', async t => {
+test('should have an error with an invalid public domain', async t => {
   let errorMessage = await execa.stdout('./cli.js', ['http://cauealves.cmo']);
   t.is(errorMessage, '\n\u001b[?25lCould not resolve the URL: http://cauealves.cmo \nPlease, check the spelling or make sure is accessible.\n\n\u001b[?25h');
 });
