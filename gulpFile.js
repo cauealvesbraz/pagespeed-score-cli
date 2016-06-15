11e21b1aef7e31297a5769d591b62a23254acb0a
tarungarg546 <tarungarg546@gmail.com>
2016-06-15 20:56:28 +0530

    2 spaces indent in gulpFile

--- gulpFile.js
+++ gulpFile.js
@@ -1,43 +1,43 @@
-"use strict";
+'use strict';
 const gulp = require('gulp');
 const notify = require('gulp-notify');
 const jscs = require('gulp-jscs');
 const jshint = require('gulp-jshint');
-const ava = require("gulp-ava");
-const babel = require("gulp-babel");
-const files = ["*.js","src/*.js"];
+const ava = require('gulp-ava');
+const babel = require('gulp-babel');
+const files = ['*.js','src/*.js'];
 
 gulp.watch(files, ['check']);
 
 gulp.task('transpile',_=> {
-     return gulp.src('src/*.js')
-                .pipe(babel({
-                    presets: ['es2015']
-                }))
-                .pipe(gulp.dest('dist'));
+  return gulp.src('src/*.js')
+              .pipe(babel({
+                presets: ['es2015']
+              }))
+              .pipe(gulp.dest('dist'));
 });
 
 gulp.task('jscs', ['transpile'],_ => {
-    return gulp.src(files)
-        .pipe(jscs());
+  return gulp.src(files)
+              .pipe(jscs());
 });
 
 gulp.task('lint', ['jscs'],_ => {
-    return gulp.src(files)
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter("fail"));   
+  return gulp.src(files)
+              .pipe(jshint())
+              .pipe(jshint.reporter('jshint-stylish'))
+              .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('test',['lint'],_=> {
-    return gulp.src('test.js')
-                .pipe(ava());
+  return gulp.src('test.js')
+              .pipe(ava());
 });
 
 gulp.task('check', ['test'], _ => {
-    gulp.src('/')
-        .pipe(notify({
-            title: 'Task Builder',
-            message: 'Successfully built application'
-        }));
+  return gulp.src('/')
+              .pipe(notify({
+                title: 'Task Builder',
+                message: 'Successfully built application'
+              }));
 });
