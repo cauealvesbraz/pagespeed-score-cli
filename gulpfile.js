197b47057f7570a7dccb767262529acc5ecc9dcc
tarungarg546 <tarungarg546@gmail.com>
2016-06-17 01:23:56 +0530

    .editorconfig added & gulp task added :whale:

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gulp = require('gulp');
+var xo = require('gulp-xo');
+var ava = require('gulp-ava');
+
+gulp.task('xo', function () {
+  return gulp.src('*.js')
+              .pipe(xo({
+                spaces: true
+              }));
+});
+
+gulp.task('ava', ['xo'], function () {
+  return gulp.src('test.js')
+              .pipe(ava());
+});
+
+gulp.task('check', ['test']);
